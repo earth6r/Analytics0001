0869f7c08850b8f4acee2a5f80d309fc13da1f7e
apinanyogaratnam <apinanapinan@icloud.com>
2024-05-28 21:19:21 -0400

    feat: progress

---
+++ src/pages/products.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/header";
+import Image from "next/image";
+
+const Products = () => {
+    return (
+        <div>
+            <Header />
+            <div className="p-6">
+                <div className="flex flex-col items-center justify-center border rounded-md min-h-screen-calc-13vh">
+                    <Image src="/coming-soon.png" className="rounded-full aspect-square object-cover" width={300} height={300} alt="Coming Soon" />
+                    <div className="text-5xl">Coming Soon!</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
