0288955c5477de9c24c36fbd413bec5b11a164ba
apinanyogaratnam <apinanapinan@icloud.com>
2024-06-01 07:14:18 -0400

    feat: progress

--- src/components/dashboard/messages/messages-stats-cards.tsx
+++ src/components/dashboard/messages/messages-stats-cards.tsx
@@ -2,14 +2,15 @@ import MessageGroupCountsStatsCard from "@/components/dashboard/messages/message
 import TotalMessagesStatsCard from "@/components/dashboard/messages/total-messages-stats-card";
 import TotalUniqueUsersStatsCard from "@/components/dashboard/messages/total-unique-users-stats-card";
 import UnansweredMessageStatsCard from "@/components/dashboard/messages/unanswered-messages-stats-card";
+import InstagramMessagesCountStatsCard from "@/components/dashboard/messages/instagram-messages-count-stats-card";
 
 const MessagesStatCards = () => {
     return (
-        <div className="grid grid-cols-1 gap-6 pl-6 pr-6 pt-2 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-6 pl-6 pr-6 pt-2 md:grid-cols-2 lg:grid-cols-5">
             <TotalMessagesStatsCard />
             <UnansweredMessageStatsCard />
             <MessageGroupCountsStatsCard />
-            {/* <InstagramMessagesCountStatsCard /> */}
+            <InstagramMessagesCountStatsCard />
             <TotalUniqueUsersStatsCard />
         </div>
     );
