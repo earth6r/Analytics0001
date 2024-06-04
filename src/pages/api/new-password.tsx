528b550d9db784b6b0393b910d41bb2670c88c64
apinanyogaratnam <apinanapinan@icloud.com>
2024-06-04 19:45:48 -0400

    feat: progress

--- src/pages/api/new-password.tsx
+++ src/pages/api/new-password.tsx
@@ -5,15 +5,15 @@ export const config = {
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
+    // if (req.method === 'OPTIONS') {
+    //     res.status(200).end();
+    //     return;
+    // }
 
     const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
 
