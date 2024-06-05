3e32312a2a168cb9da98a9cecbc4fcab43a4c0ce
apinanyogaratnam <apinanapinan@icloud.com>
2024-06-04 20:04:57 -0400

    feat: progress

--- src/pages/api/new-password.tsx
+++ src/pages/api/new-password.tsx
@@ -1,20 +1,11 @@
-import admin from 'firebase-admin';import { type NextApiRequest, type NextApiResponse } from "next";
+import admin from 'firebase-admin';
+import { type NextApiRequest, type NextApiResponse } from "next";
 
 export const config = {
     maxDuration: 300,
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    // res.setHeader('Access-Control-Allow-Credentials', 'true');
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    // res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-
-    // if (req.method === 'OPTIONS') {
-    //     res.status(200).end();
-    //     return;
-    // }
-
     const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
 
     if (!admin.apps.length) {
