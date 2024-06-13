36aadfdf5a253fd02f0086c99954664e19e9eb3f
apinanyogaratnam <apinanapinan@icloud.com>
2024-06-12 22:30:36 -0400

    feat: emails

--- src/components/dashboard/emails/emails.tsx
+++ src/components/dashboard/emails/emails.tsx
@@ -1,26 +1,34 @@
-import DeliveredEmailsChart from "@/components/dashboard/emails/delivered-emails-chart";
-import BlockedEmailsChart from "@/components/dashboard/emails/blocked-emails-chart";
-import BounceDropsEmailsChart from "@/components/dashboard/emails/bounce-drops-emails-chart";
-import EmailClicksChart from "@/components/dashboard/emails/email-clicks";
-import InvalidEmailsChart from "@/components/dashboard/emails/invalid-emails-chart";
-import OpenedEmailsChart from "@/components/dashboard/emails/opened-emails-chart";
-import ProcessedEmailsChart from "@/components/dashboard/emails/processed-emails-chart";
-import RequestsChart from "@/components/dashboard/emails/requests-chart";
-import SpamReportDropsChart from "@/components/dashboard/emails/spam-report-drops-chart";
-import UniqueClicksChart from "@/components/dashboard/emails/unique-clicks-chart";
-import UniqueOpensChart from "@/components/dashboard/emails/unique-opens-chart";
-import UnsubscribeDropsChart from "@/components/dashboard/emails/unsubscribe-drops-chart";
-import UnsubscribesChart from "@/components/dashboard/emails/unsubscribes-chart";
+// import DeliveredEmailsChart from "@/components/dashboard/emails/delivered-emails-chart";
+// import BlockedEmailsChart from "@/components/dashboard/emails/blocked-emails-chart";
+// import BounceDropsEmailsChart from "@/components/dashboard/emails/bounce-drops-emails-chart";
+// import EmailClicksChart from "@/components/dashboard/emails/email-clicks";
+// import InvalidEmailsChart from "@/components/dashboard/emails/invalid-emails-chart";
+// import OpenedEmailsChart from "@/components/dashboard/emails/opened-emails-chart";
+// import ProcessedEmailsChart from "@/components/dashboard/emails/processed-emails-chart";
+// import RequestsChart from "@/components/dashboard/emails/requests-chart";
+// import SpamReportDropsChart from "@/components/dashboard/emails/spam-report-drops-chart";
+// import UniqueClicksChart from "@/components/dashboard/emails/unique-clicks-chart";
+// import UniqueOpensChart from "@/components/dashboard/emails/unique-opens-chart";
+// import UnsubscribeDropsChart from "@/components/dashboard/emails/unsubscribe-drops-chart";
+// import UnsubscribesChart from "@/components/dashboard/emails/unsubscribes-chart";
 import BaseEmailsChart from "./base-emails-chart";
 
 const Emails = () => {
     return (
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
             {/* <DeliveredEmailsChart /> */}
             {/* <InvalidEmailsChart /> */}
             {/* <BlockedEmailsChart /> */}
             {/* <BounceDropsEmailsChart /> */}
             {/* <EmailClicksChart /> */}
+            {/* <OpenedEmailsChart /> */}
+            {/* <ProcessedEmailsChart /> */}
+            {/* <RequestsChart /> */}
+            {/* <SpamReportDropsChart /> */}
+            {/* <UniqueClicksChart /> */}
+            {/* <UniqueOpensChart /> */}
+            {/* <UnsubscribeDropsChart /> */}
+            {/* <UnsubscribesChart /> */}
             <BaseEmailsChart label="Delivered Emails" description="Delivered emails over time" valueKey="delivered" />
             <BaseEmailsChart label="Blocked Emails" description="Blocked emails over time" valueKey="blocks" />
             <BaseEmailsChart label="Bounce Drops Emails" description="Bounce drops emails over time" valueKey="bounce_drops" />
@@ -34,14 +42,6 @@ const Emails = () => {
             <BaseEmailsChart label="Unique Opens" description="Unique opens over time" valueKey="unique_opens" />
             <BaseEmailsChart label="Unsubscribe Drops" description="Unsubscribe drops over time" valueKey="unsubscribe_drops" />
             <BaseEmailsChart label="Unsubscribes" description="Unsubscribes over time" valueKey="unsubscribes" />
-            {/* <OpenedEmailsChart />
-            <ProcessedEmailsChart />
-            <RequestsChart />
-            <SpamReportDropsChart />
-            <UniqueClicksChart />
-            <UniqueOpensChart />
-            <UnsubscribeDropsChart />
-            <UnsubscribesChart /> */}
         </div>
     );
 };
