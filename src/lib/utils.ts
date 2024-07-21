4a3649fffe529fa0467d8ca45aceb0c139e515bf
apinanyogaratnam <apinanapinan@icloud.com>
2024-07-21 11:05:53 -0400

    feat: progress

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -30,7 +30,7 @@ export function convertDateString(dateString: string | null) {
   return formattedDate;
 }
 
-export function formatTimestamp(timestampStr: string, threeDigits = true) {
+export function formatTimestamp(timestampStr, threeDigits = true) {
   if (!timestampStr) {
     return null;
   }
@@ -41,12 +41,24 @@ export function formatTimestamp(timestampStr: string, threeDigits = true) {
   } else {
     date = new Date(Number(timestampStr) * 1000);
   }
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
 
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  // Convert date to EST timezone
+  const estOffset = -5 * 60 * 60 * 1000; // EST offset in milliseconds
+  const estDate = new Date(date.getTime() + estOffset);
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+    timeZone: 'America/New_York'
+  };
+
+  // Format date
+  const formatter = new Intl.DateTimeFormat('en-US', options);
+  const formattedDate = formatter.format(estDate);
+
+  // Add EST timezone to the formatted date
+  return formattedDate.replace('AM', 'AM EST').replace('PM', 'PM EST');
 }
