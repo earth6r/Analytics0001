70a1f2707fbd32a35c25bd2fee73cfd6edc10e4e
apinanyogaratnam <apinanapinan@icloud.com>
2024-07-23 12:31:28 -0400

    feat: progress

--- src/pages/bookings.tsx
+++ src/pages/bookings.tsx
@@ -4,6 +4,7 @@ import DeleteBookingAlertDialog from "@/components/bookings/delete-booking-alert
 import ViewAdditionalNotesDialog from "@/components/bookings/view-additional-notes-dialog";
 import ViewBookingDetailsDialog from "@/components/bookings/view-booking-details-dialog";
 import Header from "@/components/common/header";
+import Spinner from "@/components/common/spinner";
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -64,6 +65,7 @@ const Bookings = () => {
                     placeholder="Search bookings..."
                     className="mt-4 w-full md:w-1/4"
                     value={searchQuery}
+                    disabled // TODO: remove this after fixing search
                     onChange={(e) => {
                         setSearchQuery(e.target.value);
                         const searchQuery = e.target.value;
@@ -228,53 +230,95 @@ const Bookings = () => {
                 <div className="mt-4 block xl:hidden">
                     {getBookings.isLoading ? (
                         <div className="space-y-2 mt-4">
-                            <Skeleton className="h-8" />
+                            <Skeleton className="h-24" />
                         </div>
                     ) : (
-                        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                            {sortedData?.map((booking: any) => (
-                                <Card key={booking.id} className="">
-                                    <CardHeader>
-                                        <CardTitle className="truncate max-w-64">
-                                            {booking.type === "Property Tour" ? "Property Tour" : "Call"} with {booking.firstName || "No First Name Provided"} {booking.lastName || "No Last Name Provided"}
-                                        </CardTitle>
-                                        <CardDescription>
-                                            {formatTimestamp(booking.startTimestamp)}
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <div className="flex flex-row items-center justify-between px-6">
-                                        <div className="">
-                                            <h1 className="text-muted-foreground font-light">Duration</h1>
-                                            <div className="">{(booking?.endTimestamp - booking?.startTimestamp) / (60 * 1000) + " minutes" || "No duration set"}</div>
-                                        </div>
-                                        <div className="">
-                                            <h1 className="text-muted-foreground font-light">Scheduled By</h1>
-                                            <div className="max-w-48 truncate">{(booking?.firstName + " " + booking?.lastName)}</div>
-                                        </div>
-                                    </div>
-                                    <div className="px-6 pb-6 flex flex-row items-center justify-between mt-10 space-x-2">
-                                        <div className="flex flex-row items-center justify-center space-x-4">
-                                            <Button variant="outline" className="w-full" onClick={
-                                                () => {
-                                                    window.location.href = `tel:${booking.phoneNumber}`;
-                                                }
-                                            }>
-                                                <PhoneIcon className="w-5 h-5 mr-2" />
-                                                Call now
-                                            </Button>
-                                            <ViewBookingDetailsDialog booking={booking} />
-                                        </div>
-                                        <div className="flex flex-row items-center justify-between">
-                                            <AddAdditionalNotesDialog booking={booking} refetch={getBookings.refetch} />
-                                        </div>
-                                    </div>
-                                </Card>
+                        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+                            {sortedData?.map((booking: any, index) => (
+                                <BookingCard
+                                    key={index}
+                                    booking={booking}
+                                    completeBooking={completeBooking}
+                                    getBookings={getBookings}
+                                />
                             ))}
                         </div>
                     )}
                 </div>
             </div>
         </div>
+    );
+};
+
+interface BookingCardProps {
+    booking: any;
+    completeBooking: any;
+    getBookings: any;
+}
+
+const BookingCard = (props: BookingCardProps) => {
+    const { booking, completeBooking, getBookings } = props;
+
+    const router = useRouter();
+    const [markingCompleted, setMarkingCompleted] = useState(false);
+
+    return (
+        <Card key={booking.id} className={cn(booking?.completed ? "opacity-50" : "")}>
+            <CardHeader>
+                <CardTitle className="truncate max-w-64">
+                    {booking.type === "Property Tour" ? "Property Tour" : "Call"} with {booking.firstName || "No First Name Provided"} {booking.lastName || "No Last Name Provided"}
+                </CardTitle>
+                <CardDescription>
+                    {formatTimestamp(booking.startTimestamp)}
+                </CardDescription>
+            </CardHeader>
+            <div className="flex flex-row items-center justify-between px-6">
+                <div className="">
+                    <h1 className="text-muted-foreground font-light">Duration</h1>
+                    <div className="">{(booking?.endTimestamp - booking?.startTimestamp) / (60 * 1000) + " minutes" || "No duration set"}</div>
+                </div>
+                <div className="">
+                    <h1 className="text-muted-foreground font-light">Scheduled By</h1>
+                    <div className="max-w-48 truncate">{(booking?.firstName + " " + booking?.lastName)}</div>
+                </div>
+            </div>
+            <div className="px-6 pb-6 flex flex-row items-center justify-between mt-10 space-x-2">
+                <div className="flex flex-row items-center justify-center space-x-4">
+                    {/* <Button variant="outline" className="w-full" onClick={
+                () => {
+                    window.location.href = `tel:${booking.phoneNumber}`;
+                }
+            }>
+                <PhoneIcon className="w-5 h-5 mr-2" />
+                Call now
+            </Button> */}
+                    {/* <ViewBookingDetailsDialog booking={booking} /> */}
+                    <Button variant="default" onClick={
+                        async () => await router.push(`/booking-details?email=${booking.email}&type=${booking.type}&uid=${booking.uid}`)
+                    }>
+                        View Full Details
+                    </Button>
+                    {!booking?.completed && (
+                        <Button variant="default" onClick={
+                            async () => {
+                                setMarkingCompleted(true);
+                                await completeBooking.mutateAsync({
+                                    uid: booking.uid,
+                                    bookingType: booking.type,
+                                });
+                                await getBookings.refetch();
+                                setMarkingCompleted(false);
+                            }
+                        }>
+                            {markingCompleted ? <Spinner /> : "Mark as Completed"}
+                        </Button>
+                    )}
+                </div>
+                <div className="flex flex-row items-center justify-between">
+                    <AddAdditionalNotesDialog booking={booking} refetch={getBookings.refetch} />
+                </div>
+            </div>
+        </Card>
     );
 };
 
