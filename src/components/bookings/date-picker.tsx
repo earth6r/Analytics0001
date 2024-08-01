42ac4d1fc9b81180550ed991bf74e088913a9758
apinanyogaratnam <apinanapinan@icloud.com>
2024-08-01 17:53:41 -0400

    feat: progress

---
+++ src/components/bookings/date-picker.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+
+interface DatetimePickerProps {
+    placeholder?: string;
+    value: Date;
+    onValueChange: (value: Date) => void;
+}
+
+export function DatePicker(props: DatetimePickerProps) {
+    const { placeholder = "Pick a date", value, onValueChange } = props;
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant={"outline"}
+                    className={cn(
+                        "w-full justify-start text-left font-normal",
+                        !value && "text-muted-foreground"
+                    )}
+                >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {value ? format(value, "PPP") : <span>{placeholder}</span>}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0 z-[100] pointer-events-auto">
+                <Calendar
+                    mode="single"
+                    selected={value}
+                    onSelect={(date) => {
+                        onValueChange(date as Date);
+                    }}
+                />
+            </PopoverContent>
+        </Popover>
+    )
+}
