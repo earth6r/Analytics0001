b51979e4417526c49a95e8d41870c2aabbe4b73f
apinanyogaratnam <apinanapinan@icloud.com>
2024-08-01 11:58:36 -0400

    feat: progress

--- src/pages/bookings.tsx
+++ src/pages/bookings.tsx
@@ -304,7 +304,7 @@ const Bookings = () => {
                                 </pre> */}
                                 <StatusSelect value={booking?.status ? booking.status : "scheduled"} onChange={
                                     async (value: string) => {
-                                        if (!booking?.status || value === "scheduled") return;
+                                        if (!value || value === "scheduled") return;
                                         setLoadingsForStatuses({
                                             ...loadingsForStatuses,
                                             [booking.uid]: true,
@@ -423,7 +423,7 @@ const BookingCard = (props: BookingCardProps) => {
                     {/* <div className="">{(booking?.completed ? "completed" : "scheduled")}</div> */}
                     <StatusSelect value={booking?.status ? booking.status : "scheduled"} onChange={
                         async (value: string) => {
-                            if (!booking?.status || value === "scheduled") return;
+                            if (!value || value === "scheduled") return;
                             setStatusLoading(true);
                             await updateBookingStatus.mutateAsync({
                                 uid: booking.uid,
