718b6318a2e136cf13120c93d7ed51db10f296b0
YannyD <yan@earth.net>
2024-08-29 23:33:41 -0500

    feat: change tour confirmation text

--- src/pages/api/cron/notify-property-tour-bookings-via-text.ts
+++ src/pages/api/cron/notify-property-tour-bookings-via-text.ts
@@ -6,74 +6,81 @@ import moment from "moment-timezone";
 import { type NextApiRequest, type NextApiResponse } from "next/types";
 
 const sendNotification = async (
-    email: string,
-    firstName: string,
-    lastName: string,
-    phoneNumber: string,
-    startTimestamp: string,
+  email: string,
+  firstName: string,
+  lastName: string,
+  phoneNumber: string,
+  startTimestamp: string,
 ) => {
-    // try {
-    //     await axios.post(`${API_URL}/send-phone-call-reminder`, {
-    //         // TODO: change recipientPhone to all numbers of team
-    //         recipientPhone: phoneNumber,
-    //         email,
-    //         firstName,
-    //         lastName,
-    //         phoneNumber,
-    //         startTimestamp,
-    //     })
-    // } catch (error) {
-    //     console.error(`Failed to send notification to ${phoneNumber}`, error);
-    // }
+  // try {
+  //     await axios.post(`${API_URL}/send-phone-call-reminder`, {
+  //         // TODO: change recipientPhone to all numbers of team
+  //         recipientPhone: phoneNumber,
+  //         email,
+  //         firstName,
+  //         lastName,
+  //         phoneNumber,
+  //         startTimestamp,
+  //     })
+  // } catch (error) {
+  //     console.error(`Failed to send notification to ${phoneNumber}`, error);
+  // }
 
-    try {
-        await axios.post(`${API_URL}/send-message`, {
-            recipientPhone: phoneNumber,
-            message:
-                `Hi ${firstName}, Just a quick reminder about your upcoming tour at 48 Allen, New York, NY, 10002 at ${startTimestamp}. You will meet Carl outside. Let us know if you are unable to make it.
-Best,
-Talin`,
-        })
-    } catch (error) {
-        console.error(`Failed to send notification to ${phoneNumber}`, error);
+  try {
+    await axios.post(`${API_URL}/send-message`, {
+      recipientPhone: phoneNumber,
+      message: `Hi ${firstName}, Just a quick reminder about your upcoming tour of HOME0001 Lower East Side  tomorrow at ${startTimestamp}. You’ll meet Carl in front of the building at 48 Allen (btw. Hester and Grand). Please give us a heads up if you’re running late or want to reschedule.`,
+    });
+  } catch (error) {
+    console.error(`Failed to send notification to ${phoneNumber}`, error);
+  }
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const propertyTourRef = collection(db, "usersBookPropertyTour");
+
+  // Get all property tour bookings
+  const propertyTourSnapshot = await getDocs(propertyTourRef);
+
+  const propertyTours = propertyTourSnapshot.docs.map((doc) => doc.data());
+
+  for (const propertyTour of propertyTours) {
+    // Convert startTimestamp to a moment object in UTC
+    const startTimestampEpochUTC = Number(propertyTour.startTimestamp);
+    const propertyTourDate = moment
+      .utc(startTimestampEpochUTC)
+      .startOf("minute"); // round down to nearest minute
+
+    // Get the current time in UTC and round down to nearest minute
+    const currentDate = moment.utc().startOf("minute");
+
+    // Check if the property tour is scheduled in 30 minutes
+    const minutesUntilTour = propertyTourDate.diff(currentDate, "minutes");
+
+    const startTimestampEst = moment
+      .utc(startTimestampEpochUTC)
+      .tz("America/New_York")
+      .format("YYYY-MM-DD HH:mm:ss");
+    // console.error(
+    //     `current date: ${currentDate.format('YYYY-MM-DD HH:mm:ss')}, start date: ${propertyTourDate.format('YYYY-MM-DD HH:mm:ss')}, minutes until tour: ${minutesUntilTour}`,
+    // )
+
+    // 24 hours before the tour
+    if (minutesUntilTour === 60 * 24) {
+      await sendNotification(
+        propertyTour.email,
+        propertyTour.firstName,
+        propertyTour.lastName,
+        propertyTour.phoneNumber,
+        startTimestampEst,
+      );
     }
-}
+  }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const propertyTourRef = collection(db, "usersBookPropertyTour");
-
-    // Get all property tour bookings
-    const propertyTourSnapshot = await getDocs(propertyTourRef);
-
-    const propertyTours = propertyTourSnapshot.docs.map(doc => doc.data());
-
-    for (const propertyTour of propertyTours) {
-        // Convert startTimestamp to a moment object in UTC
-        const startTimestampEpochUTC = Number(propertyTour.startTimestamp);
-        const propertyTourDate = moment.utc(startTimestampEpochUTC).startOf('minute'); // round down to nearest minute
-
-        // Get the current time in UTC and round down to nearest minute
-        const currentDate = moment.utc().startOf('minute');
-
-        // Check if the property tour is scheduled in 30 minutes
-        const minutesUntilTour = propertyTourDate.diff(currentDate, 'minutes');
-
-        const startTimestampEst = moment.utc(startTimestampEpochUTC).tz('America/New_York').format('YYYY-MM-DD HH:mm:ss');
-        // console.error(
-        //     `current date: ${currentDate.format('YYYY-MM-DD HH:mm:ss')}, start date: ${propertyTourDate.format('YYYY-MM-DD HH:mm:ss')}, minutes until tour: ${minutesUntilTour}`,
-        // )
-
-        // 24 hours before the tour
-        if (minutesUntilTour === 60 * 24) {
-            await sendNotification(
-                propertyTour.email,
-                propertyTour.firstName,
-                propertyTour.lastName,
-                propertyTour.phoneNumber,
-                startTimestampEst,
-            );
-        }
-    }
-
-    res.status(200).json({ message: "Notifications checked and sent if needed." });
+  res
+    .status(200)
+    .json({ message: "Notifications checked and sent if needed." });
 }
